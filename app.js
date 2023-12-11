928259a1ce4ff187b9fc8b4faca01dde95ed9917
Juan <juan.laredo@utec.edu.pe>
2023-12-10 19:26:14 -0500

    Pushing proyect

---
+++ app.js
@@ -0,0 +1,19 @@
+(function(){
+    'use strict';
+    angular.module('app',[])
+    .controller('controller',LunchController);
+    LunchController.$inject = ['$scope'];
+    function LunchController($scope){
+        $scope.name = '';
+        $scope.message = '';
+        $scope.checkbutton= () =>{
+            let splitterms = $scope.name.split(',');
+            if(splitterms[0].trim() === ''){
+                $scope.message = "Please enter data first";
+            } 
+            else if(splitterms.length <= 3) 
+                $scope.message = "Enjoy!";
+            else $scope.message = "Too Much!";
+        }
+    }
+})();
